5935dabe712a077a20a6e76fe7be35464300e826
jpmikkers <jean.paul.mikkers@gmail.com>
2025-02-18 13:30:17 +0100

    target net 8

--- Baksteen.Extensions.WhereNotNull/IEnumerableExtensions.cs
+++ Baksteen.Extensions.WhereNotNull/IEnumerableExtensions.cs
@@ -1,30 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Baksteen.Extensions.WhereNotNull
-{
-    public static class IEnumerableExtensions
-    {
-        /// <summary>
-        /// Skips all null elements from the given sequence. This is the overload that handles nullable reference types.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of source</typeparam>
-        /// <param name="source">An <see cref="IEnumerable{T}"/> to filter</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> of non-null elements</returns>
-        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class =>
-            from item in source
-            where item != null
-            select item;
+namespace Baksteen.Extensions.WhereNotNull;
 
-        /// <summary>
-        /// Skips all null elements from the given sequence. This is the overload that handles nullable value types.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of source</typeparam>
-        /// <param name="source">An <see cref="IEnumerable{T}"/> to filter</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> of non-null elements</returns>
-        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : struct =>
-            from item in source
-            where item.HasValue
-            select item.Value;
-    }
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// Skips all null elements from the given sequence. This is the overload that handles nullable reference types.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of source</typeparam>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to filter</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of non-null elements</returns>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class =>
+        from item in source
+        where item != null
+        select item;
+
+    /// <summary>
+    /// Skips all null elements from the given sequence. This is the overload that handles nullable value types.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of source</typeparam>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to filter</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of non-null elements</returns>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : struct =>
+        from item in source
+        where item.HasValue
+        select item.Value;
 }
